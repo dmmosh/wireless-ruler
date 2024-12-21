b5499064d45bb17a57890974566303ab12b3b4a7
wettestsock <evilsharkeater@gmail.com>
2024-12-21 16:58:10 -0500

    nothing of note

--- wr1/src/main.cpp
+++ wr1/src/main.cpp
@@ -1,18 +1,40 @@
 #include <Arduino.h>
+#include "BLEScan.h"
+#include "BLEAdvertisedDevice.h"
+#include "BLEDevice.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#define LED 2
+#define CUTOFF -60
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    pinMode(LED,OUTPUT);
+    BLEDevice::init("");
+}
+
+void led_blink(void* args){
+    pinMode(LED,HIGH);
+    vTaskDelay(500/portTICK_PERIOD_MS);
+    pinMode(LED,LOW);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    BLEScan* scan = BLEDevice::getScan();
+    scan->setActiveScan(true);
+    BLEScanResults results = scan->start(1);
+    int count = results.getCount();
+    if (count == 0){
+        Serial.println("No devices found.");
+    }
+    for (size_t i = 0; i < count; i++)
+    {
+        BLEAdvertisedDevice device = results.getDevice(i);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+        Serial.printf("%i %s\n", device.getRSSI(),device.getName());
+        xTaskCreate(led_blink,"led blink", 512,NULL,1,NULL);
+
+    }
+
+  // put your main code here, to run repeatedly:
 }
