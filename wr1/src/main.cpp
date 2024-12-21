6db0486d57f72c340d3dafc955db5970e9b13fa5
wettestsock <evilsharkeater@gmail.com>
2024-12-21 17:38:25 -0500

    nothing of note

--- wr1/src/main.cpp
+++ wr1/src/main.cpp
@@ -17,17 +17,17 @@ void loop() {
     BLEScan* scan = BLEDevice::getScan();
     scan->setActiveScan(true);
     BLEScanResults results = scan->start(1);
-    int best = CUTOFF;
-    digitalWrite(LED,LOW);
+    bool other_esp = false;
     for (size_t i = 0; i < results.getCount(); i++)
     {
         BLEAdvertisedDevice device = results.getDevice(i);
 
         if (!strcmp(device.getServiceDataUUID().toString().c_str(), "9f46b94c-9574-4f6c-bd1b-ddc3a7a83a43")){
-            digitalWrite(LED, HIGH);
+            other_esp = true;
         }
         //Serial.printf("%i %s %s\n", device.getRSSI(),device.getName().c_str(), device.getServiceUUID().toString().c_str());
     }
+    digitalWrite(LED,(other_esp) ? HIGH : LOW);
     //digitalWrite(LED, (best>CUTOFF) ? HIGH : LOW);
 
   // put your main code here, to run repeatedly:
